1333473b933a83cec3af4d529e6eedd2efb7f4ae
Gleb Sukristik <Gleb15a@gmail.com>
2022-03-06 23:44:08 +0300

    Created a map, wich will rebuild our Command model to CommandReadDto model

---
+++ Commander/Mappers/CommandsMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Commander.DTOs;
+using Commander.Models;
+
+namespace Commander.Mappers
+{
+    public class CommandsMapper : Profile
+    {
+        public CommandsMapper()
+        {
+            CreateMap<Command, CommandReadDto>();
+        }
+    }
+}
