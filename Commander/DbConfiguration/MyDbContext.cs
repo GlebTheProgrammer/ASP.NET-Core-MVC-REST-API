619d4f6cfd7e1f4e9ab008ffde1235a84181b4e9
Gleb Sukristik <Gleb15a@gmail.com>
2022-03-06 16:50:11 +0300

    Added DbContext class and connection string realization

---
+++ Commander/DbConfiguration/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Commander.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Commander.DbConfiguration
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Command> Commands { get; set; }
+    }
+}
--- Commander/Startup.cs
+++ Commander/Startup.cs
@@ -1,9 +1,11 @@
+using Commander.DbConfiguration;
 using Commander.domain;
 using Commander.Interfaces;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,8 @@ namespace Commander
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MyDbContext>(opt => opt.UseSqlServer
+            (Configuration.GetConnectionString("CommanderConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
