71d3be0afac7d6db3bcf5a0a027af0cdf9973771
Gleb Sukristik <Gleb15a@gmail.com>
2022-03-07 00:00:44 +0300

    Implemented Data Transfer Object into controller class, and return CommandReadDto instead of hole Command object right now

--- Commander/Controllers/CommandsController.cs
+++ Commander/Controllers/CommandsController.cs
@@ -1,4 +1,6 @@
-﻿using Commander.domain;
+﻿using AutoMapper;
+using Commander.domain;
+using Commander.DTOs;
 using Commander.Interfaces;
 using Commander.Models;
 using Microsoft.AspNetCore.Mvc;
@@ -14,29 +16,31 @@ namespace Commander.Controllers
     public class CommandsController : ControllerBase
     {
         private readonly ICommanderRepository repository;
+        private readonly IMapper mapper;
 
-        public CommandsController(ICommanderRepository repository)
+        public CommandsController(ICommanderRepository repository, IMapper mapper)
         {
             this.repository = repository;
+            this.mapper = mapper;
         }
 
         // GET api/commands
         [HttpGet]
-        public ActionResult <IEnumerable<Command>> GetAllCommand()
+        public ActionResult <IEnumerable<CommandReadDto>> GetAllCommand()
         {
             var commandItems = repository.GetAllCommands();
 
-            return Ok(commandItems);
+            return Ok(mapper.Map<IEnumerable<CommandReadDto>>(commandItems));
         }
 
         // GET api/commands/5
         [HttpGet("{id}")]
-        public ActionResult <Command> GetCommandById(int id)
+        public ActionResult <CommandReadDto> GetCommandById(int id)
         {
             var commandItem = repository.GetCommandById(id);
 
             if (commandItem != null)
-                return Ok(commandItem);
+                return Ok(mapper.Map<CommandReadDto>(commandItem));
 
             return NotFound();
         }
