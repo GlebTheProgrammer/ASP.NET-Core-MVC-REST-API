a52106099a158c169d5b4a89991cfdc4f02e33fe
Gleb Sukristik <Gleb15a@gmail.com>
2022-03-06 23:36:40 +0300

    Added CommandReadDto implementation (it's what the user will see when he will ask for the response. Right there, i just deleted platform property. It means, that in future user will not see this information)

---
+++ Commander/DTOs/CommandReadDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Commander.DTOs
+{
+    public class CommandReadDto
+    {
+        public int Id { get; set; }
+        public string HowTo { get; set; }
+        public string Line { get; set; }
+    }
+}
